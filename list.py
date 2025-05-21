ae2d7e193d7fe44b4bf74a202ee32637363dc351
abhiramchakka <113310632+abhiramchakka@users.noreply.github.com>
2025-05-21 12:07:15 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,57 @@
+# list-list is a collection of multiple item into single unit.list are ordered amd list allow duplicates also.and list are mutable.
+# mutable means we can modify over list after create also
+# a=[10,23,4,-12,23,90,4,5,83,6,7,34,8,9]
+# # b=[10,'abhiram',True,-79]
+# print(a)
+# print(b)
+# print(a[2])
+# print(b[2])
+# print(a[3])
+# print(a[0:9])
+# print(a[3:9])
+# print(a[0:9:2])
+# print(a[0::2])
+# print(a[::])
+# print(a[::-2])
+# # a.sort()
+# # print(a)
+# # a.reverse()
+# # print(a)
+# print(len(a))
+# print(min(a))
+# print(max(a))
+# a.append(12)
+# print(a)
+# a.insert(3,90)
+# print(a)
+# b.insert(3,"abhiram")
+# print(b)
+# a.extend([12,3,2,4,5])
+# print(a)
+# print(len(a))
+# a[2]=23
+# print(a)
+a=[10,23,4,-12,23,90,4,5,83,6,7,34,8,9]
+# a[1:4]=[12,3,4]
+# print(a)
+# a.remove(23)
+# print(a)
+# a.remove(90)
+# print(a)
+# a.pop()
+# print(a)
+# print(a.count(23))
+
+
+# for i in a:
+#     print(i)
+
+
+# tuples in python
+tuple1=(23,23,4,5,[34,34],67,6,6,7,8,90)
+# for i in  tuple1:
+#     print(i)
+
+# tuple1[1]=23
+# print(tuple1) it well showes a  type error
+
